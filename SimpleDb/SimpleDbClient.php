30a37f39a4833761b82cfff0df1520b2c57cca6f
Michael Dowling <mtdowling@gmail.com>
2011-03-22 15:34:45 -0500

    Updating aws config references

--- SimpleDb/SimpleDbClient.php
+++ SimpleDb/SimpleDbClient.php
@@ -17,14 +17,6 @@ use Guzzle\Service\Aws\Signature\SignatureV2;
  * Client for interacting with Amazon SimpleDb
  *
  * @author Michael Dowling <michael@guzzlephp.org>
- *
- * @guzzle access_key_id required="true" doc="AWS Access Key ID"
- * @guzzle secret_access_key required="true" doc="AWS Secret Access Key"
- * @guzzle protocol required="true" default="http" doc="Protocol to use with requests (http or https)"
- * @guzzle region required="true" default="sdb.amazonaws.com" doc="Amazon SimpleDB Region endpoint"
- * @guzzle base_url required="true" default="{{ protocol }}://{{ region }}/" doc="SimpleDB service base URL"
- *
- * @guzzle cache.key_filter static="query=Timestamp, Signature"
  */
 class SimpleDbClient extends AbstractClient
 {
