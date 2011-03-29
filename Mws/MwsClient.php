e7a7d626c953d0fb85bcc7a6ab2c234eb02bea02
Harold Asbridge <hasbridge@gmail.com>
2011-03-29 11:46:35 -0500

    Disabling exponential backoff plugin in MWS client

--- Mws/MwsClient.php
+++ Mws/MwsClient.php
@@ -68,7 +68,7 @@ class MwsClient extends AbstractClient
         );
 
         // Retry 500 and 503 failures using exponential backoff
-        $client->getEventManager()->attach(new ExponentialBackoffPlugin());
+        //$client->getEventManager()->attach(new ExponentialBackoffPlugin());
 
         return $client;
     }
